19b721fa00d9997e061f5f57518329741c76ebca
王宇晗 <ousakai@wangyuhandeMacBook-Pro.local>
2019-06-11 23:07:06 +0800

    chu

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'music'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,57 @@
+<template>
+<div id="window-top">
+  <div class="header-bar-button">
+    <!-- <div class="buttons">
+      <a class="btn closes" title="关闭" @click="clickItem('window-close')"></a>
+      <a class="btn minimize" title="最小化" @click="clickItem('window-min')"></a>
+      <a class="btn maximize" title="最大化" @click="clickItem('window-max')"></a>
+    </div> -->
+</div>
+</div>
+</template>
+
+<script>
+import { ipcRenderer } from 'electron';
+  export default {
+    methods: {
+      clickItem(item) {
+        ipcRenderer.send(item)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scope>
+#window-top {
+  -webkit-app-region: drag;
+  background-color: #fff;
+}
+  .header-bar-button {
+  position: relative;
+  font-size: 0;
+  z-index: 200;
+  padding: 3px 5px;
+  .btn {
+    -webkit-app-region: no-drag;
+    display: inline-block;
+    position: relative;
+    padding: 0;
+    border: none;
+    box-shadow: 0 0 1px rgba(0,0,0,0.3);
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    margin: 0 4px;
+    cursor: pointer;
+    &.closes {
+      background-color: #fc625d;
+    }
+    &.minimize {
+      background-color: #fdbc40;
+    }
+    &.maximize {
+      background-color: #34c84a;
+    }
+  }
+}
+</style>
