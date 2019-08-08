8d8863b6907837a249bea95c99122f2031c9a573
wyhxx <wangyuhan328@163.com>
2019-08-08 18:08:52 +0800

    zz

--- src/renderer/views/discoverMusic/songList/index.vue
+++ src/renderer/views/discoverMusic/songList/index.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-歌单213
+  <div class="playlist-wrapper">
+    <div class="playlist-header">
+      <el-popover
+        placement="top-start"
+        popper-class="playlist-all"
+        trigger="click"
+        >
+        <div class="more-category-wrapper">
+          <div class="cat-title">
+            <a href="#">全部歌单</a>
+          </div>
+        </div>
+         <a href="#" class="btn" slot="reference">
+            <span>流行</span>
+            <i class="el-icon-arrow-right"></i>
+          </a>
+      </el-popover>
+      <div class="playlist-hot">
+        <!-- <span>雨纷纷</span>
+    <el-divider direction="vertical"></el-divider>
+    <span>旧故里</span>
+    <el-divider direction="vertical"></el-divider>
+    <span>草木深</span> -->
+        <ul class="hot-list">
+          <li class="hot-list-item" 
+          v-for="(item,index) in hotCategoryList" :key="index">
+            <a href="#" class="item-name">
+              <span>{{item.name}}</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +42,8 @@ import to from "@/utils/await-to.js"
     name:'songList',
     data() {
       return {
+        allCategoryList: [],
+        hotCategoryList: []
       }
     },
     created(){
@@ -23,6 +56,7 @@ import to from "@/utils/await-to.js"
     methods: {
       async getPlaylistHotCategory() {
         let [res] = await to(neteaseApi.playlistHot())
+        this.hotCategoryList = res.tags
         console.log('热门分类--',res)
       },
       async getPlaylistAllCategory() {
@@ -34,5 +68,72 @@ import to from "@/utils/await-to.js"
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/style/mixin.scss";
+@import "@/assets/style/variables.scss";
 
+.playlist-all {
+  
+  .more-category-wrapper {
+    .cat-title {
+      padding: 15px;
+      color: #000;
+    }
+  }
+}
+
+.playlist-wrapper {
+
+  .playlist-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 30px;
+
+    .btn {
+      display: block;
+      padding: 6px 20px;
+      border-radius: 30px;
+      border:1px solid $color-border;
+      font-size: 13px;
+      color: $color-base-grey;
+    }
+
+    .playlist-hot {
+      .hot-list {
+        .hot-list-item {
+          display: inline-block;
+          &:last-child{
+            .item-name {
+              border-right: none;
+            }
+          }
+          .item-name {
+            padding: 0 7px;
+            border-right: 1px solid $color-border;
+            font-size: 12px;
+            color: $color-simple-black;
+            span {
+              display: inline-block;
+              padding: 3px 7px;
+              border-radius: 14px;
+              &.active {
+                color: $color-base-red;
+                background: rgba($color: #000, $alpha: 0.1)
+              }
+              &:hover {
+                color: $color-base-red;
+                background: rgba($color: #000, $alpha: 0.1)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+<style>
+.el-popover {
+  padding: 0;
+}
 </style>
