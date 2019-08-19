16dc4e6a45493b7070ddf17245418b5cb5e3ecf8
王宇晗 <ousakai@wangyuhandeMacBook-Pro.local>
2019-08-19 09:05:38 +0800

    11

--- src/renderer/views/UserDetail/index.vue
+++ src/renderer/views/UserDetail/index.vue
@@ -8,31 +8,40 @@
         <div class="info">
           <h4>{{userInfo.nickname}}</h4>
           <div class="tools">
-            <div class="sex">
+            <div class="btn-group">
               <span class="level">LV {{level}}</span>
               <span class="sex">
-                <i class=""></i>
+                <i class="iconfont" :class="getGender(userInfo.gender)"></i>
               </span>
             </div>
-            <div class="">
-              <span>关注</span>
+            <div class="follow-wrapper">
+              <span @click="toggleFollow" class="follow">
+                <i class="el-icon-plus"></i>
+                关注
+              </span>
             </div>
           </div>
           <div class="event">
             <ul class="list">
               <li class="item">
-                <p>0</p>
-                <p>动态</p>
+                <span>{{userInfo && userInfo.eventCount}}</span>
+                <span>动态</span>
               </li>
               <li class="item">
-                <p>10</p>
-                <p>关注</p>
+                <span>{{userInfo && userInfo.follows}}</span>
+                <span>关注</span>
               </li>
               <li class="item">
-                <p>40</p>
-                <p>粉丝</p>
+                <span>{{userInfo &&userInfo.followeds}}</span>
+                <span>粉丝</span>
               </li>
             </ul>
+          </div>
+          <div class="desc">
+            <p>
+              <span>个人介绍:</span>
+              {{userInfo.description || '暂无介绍'}}
+            </p>
           </div>
         </div>
       </div>
@@ -67,29 +76,106 @@
         this.userInfo = res.profile
         this.level = res.level
         console.log(res)
-      }
+      },
+      // 获取性别
+      getGender(gender) {
+          return gender === 0 ? '' : (gender === 1 ? 'icon-nan' : 'icon-nv')
+      },
     },
   }
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/style/variables.scss";
+
 .user-detail-wrapper {
 
   .user-info-wrapper {
     display: flex;
 
     .cover-img {
-      width: 230px;
-      height: 230px;
-      padding: 20px;
+      width: 200px;
+      height: 200px;
+      padding: 20px 30px;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
     }
     .info {
       flex:1;
-      
+      padding: 20px 20px 20px 0;
+      h4 {
+        margin: 0;
+        font-size: 23px;
+      }
       .tools {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
+        align-items: center;
+        .btn-group {
+          .level {
+            background: $background-undertintgrey;
+            padding: 2px 10px;
+            font-size: 12px;
+          }
+          .sex {
+            .iconfont {
+              font-size: 12px;
+              padding: 2px 10px;
+              border-radius: 10px;
+            }
+            .icon-nv {
+              background: rgb(255, 193, 227);
+              color: rgb(220, 43, 113);
+            }
+            .icon-nan {
+              background: rgb(192, 240, 255);
+              color: rgb(48, 141, 197);
+            }
+          }
+          
+        }
+        .follow-wrapper {
+          width: 95px;
+          text-align: center;
+          height: 100%;
+          font-size: 14px;
+          span {
+            display: inline-block;
+            height: 30px;
+            line-height: 30px;
+            font-size: $font-size-text-small;
+            border-radius: 28px;
+            padding: 0 14px;
+            background: rgb(252, 243, 244);
+            &.follow {
+              color: $color-base-red;
+              cursor: pointer;
+            }
+            &.followed {
+              color: $color-base-grey;
+            }
+          }
+        }
+      }
+
+      .event {
+        display: flex;
+        .list {
+          width: 240px;
+          display: flex;
+          flex-direction: row;
+          .item {
+            flex: 1;
+            height: 80px;
+            line-height: 80px;
+            border-right: 1px solid;
+            text-align: center;
+          }
+        }
       }
     }
   }
